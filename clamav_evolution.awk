fc544f57f909c5146ac681781a08a0cca222f04e
Felix Almeida <white-glider@techwords.io>
2018-09-24 01:07:09 -0400

    added scripts(shell and awk).

---
+++ clamav_evolution.awk
@@ -0,0 +1,14 @@
+#!/bin/awk -f
+
+BEGIN { from=subj="" }
+{
+	if (match($0, "^From: ")) {
+		if (!from) from=substr($0, RSTART) "\n"
+		next
+ 	} else if (match($0, "^Subject: ")) {
+		if (!subj) subj=substr($0, RSTART) "\n"
+		next
+	}
+	if (from && subj) exit
+}
+END { printf("%s%s", from, subj) }
---
+++ clamav_evolution.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+umask 077
+RSLT=/tmp/$$_clamav.tmp
+MSG=/tmp/$$_email.tmp
+
+function awk_file {
+	AWK_FILE=$(echo $1 | sed 's/\(.*\)\.\([^\.]*\)/\1\.awk/')
+        if [ "$1" == "$AWK_FILE" ]; then
+		AWK_FILE=$1.awk
+	fi
+}
+
+cat > $MSG
+awk_file $(readlink -f "$0")
+if [ ! -x "$AWK_FILE" ]; then
+	echo "$(basename $0): Missing AWK counterpart ($AWK_FILE)" >&2
+	rm $MSG
+	exit 2
+fi
+
+clamscan --no-summary --detect-pua=yes --official-db-only=yes $MSG > $RSLT
+if [ $? -eq 1 ]; then
+	THREAT=$(awk '$NF ~ /FOUND/ {print $2}' $RSLT)
+	STRING=$(awk -f $AWK_FILE $MSG)
+	notify-send --urgency=critical --category=email --icon=/usr/share/icons/Adwaita/scalable/status/dialog-warning-symbolic.svg "ClamAV: e-mail threat detected!" "$THREAT\n$STRING"
+	rm $MSG $RSLT
+	exit 1
+fi
+rm $MSG $RSLT
+exit 0
